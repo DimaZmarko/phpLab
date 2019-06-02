f0783a57be8bfbc803902e98857d94f7eb1a006e
DimaZmarko <dzmarko98@gmail.com>
2019-06-03 01:37:11 +0300

    Client and Admin functionality using Symfony (without PHPUnit)

---
+++ Pet-Project/src/Controller/ClientController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Answer;
+use App\Entity\Quiz;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ClientController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(Request $request)
+    {
+        $quizRepository = $this->getDoctrine()
+            ->getRepository(Quiz::class);
+
+        $search = $request->get('search');
+        if ($search) {
+            $quizes = $quizRepository->search($search);
+
+        } else {
+            $quizes = $quizRepository->findAll();
+        }
+
+        return $this->render('quiz/index.html.twig', [
+            'quizes' => $quizes
+        ]);
+    }
+
+    /**
+     * @Route("/quiz/{id}", name="quizById", requirements={"id"="\d+"})
+     */
+    public function quizById($id)
+    {
+        $quiz = $this->getDoctrine()
+            ->getRepository(Quiz::class)
+            ->find($id);
+
+        $questions = $quiz->getQuestions()->toArray();
+
+        return $this->render('quiz/singleQuiz.html.twig', [
+            'quiz' => $quiz,
+            'questions' => $questions
+        ]);
+    }
+
+    /**
+     * @Route("/quiz_result/", name="quizResult")
+     */
+    public function quizResult(Request $request)
+    {
+        $questionId = $request->get('questionId');
+
+        $result = 0;
+
+        $answerRepository = $this->getDoctrine()
+            ->getRepository(Answer::class);
+
+        foreach ($questionId as $question) {
+
+            $answer = $answerRepository->findOneBy(array('correct' => 1, 'question' => $question));
+
+            if ($answer->getId() == $request->get('question_' . $question)) {
+                $result++;
+            }
+        }
+
+        return $this->render('quiz/quizResult.html.twig', [
+            'result' => $result
+        ]);
+    }
+}
